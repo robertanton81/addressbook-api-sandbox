25196d746978153483a27b682e7e4fc922c7d836
Robert Anton <mr.robert.anton@gmail.com>
2022-10-19 21:59:27 +0200

    hook user, db modules to app module, setup config validations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
+import { dbConfig } from './database/database.config';
+import { appConfig, validateAppConfig } from './app.config';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [appConfig, dbConfig],
+      cache: true,
+      validate: validateAppConfig,
+    }),
+    DatabaseModule,
+    UsersModule,
+  ],
   controllers: [],
   providers: [],
 })
